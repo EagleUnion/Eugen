bce39c50c1f7dfb1755f474c2a2936653614f2de
Skadi <63096193+Dank-del@users.noreply.github.com>
2020-06-15 21:12:16 +0530

    Update global_bans.py
    As Simon said

--- tg_bot/modules/global_bans.py
+++ tg_bot/modules/global_bans.py
@@ -300,7 +300,7 @@ you and your groups by removing spam flooders as quickly as possible. They can b
 
 Eugen also integrates @Spamwatch API into gbans to remove Spammers as much as possible from your chatroom!
 *What is SpamWatch?*
-SpamWatch maintains a large constantly updated ban-list of spambots, trolls, bitcoin spammers and unsavoury characters[.](https://telegra.ph/file/ac12a2c6b831dd005015b.jpg)
+SpamWatch maintains a large constantly updated ban-list of spambots, bitcoin spammers and unsavoury characters[.](https://telegra.ph/file/ac12a2c6b831dd005015b.jpg)
 Eugen will constantly help banning spammers off from your group automatically So, you don't have to worry about spammers storming your group.
 """
 
